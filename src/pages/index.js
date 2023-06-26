1622dfbe02cfb10f46c643b80a27a48042b77505
Diana Soria <dianalaurasoria436@gmail.com>
2023-06-26 16:15:27 -0600

    Integración del i18n

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from "react"
-import { Call, Header, Portafolio } from "../components"
+import { Header } from "../components"
+import {graphql} from 'gatsby';
 
 const IndexPage = () => {
   return (
@@ -15,3 +16,16 @@ export default IndexPage
 
 export const Head = () => <title>Home Page</title>
 
+export const query = graphql`
+  query($language: String!) {
+    locales: allLocale(filter: {language: {eq: $language}}) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+  }
+`;
