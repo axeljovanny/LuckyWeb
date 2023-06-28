4389bc5c01775e1d8e772b0d87085d56e99727e4
Mario <lmariogc41@gmail.com>
2023-06-28 16:47:46 -0600

    se agrega diseño servicios

--- src/components/servicios.js
+++ src/components/servicios.js
@@ -1,21 +1,74 @@
-
-import React from "react"
-import { SContenedorGrid, SContenedorInicial, SContenedorTitulo } from "../styles/js/servicios";
-import {  } from "./imagesComponets";
-
-
+import React from "react";
+import {
+  SBranding,
+  SBrandingMas,
+  SBrandingTexto,
+  SContenedorGrid,
+  SContenedorInicial,
+  SContenedorTitulo,
+  SCreacionContenido,
+  SCreacionMas,
+  SCreaciontexto,
+  SDesarrolloMas,
+  SDesarrolloTexto,
+  SDesarrolloWeb,
+} from "../styles/js/servicios";
+import {} from "./imagesComponets";
+import { Plus } from "../images/icons/icons";
+import "../styles/css/svg.css";
+import { colors } from "../utils/const";
 
 const Call = ({ siteTit }) => {
-    return (
-     
-<SContenedorInicial>
+  return (
+    <SContenedorInicial>
+      <SContenedorTitulo>
+        <h1> SERVICIOS </h1>
+      </SContenedorTitulo>
+      <SContenedorGrid>
+        <SBranding>
+          {" "}
+          <h2> BRANDING</h2>
+        </SBranding>
+        <SBrandingTexto>
+          <h3>
+            {" "}
+            lorem ipsum dolor sit amet, consectetur adipsing elit, sed do
+            eiusmod tempor incididut.{" "}
+          </h3>
+        </SBrandingTexto>
+        <SBrandingMas>
+          <Plus stroke={colors.black} className={"svgPlus"} />
+        </SBrandingMas>
+        <SDesarrolloWeb>
+          {" "}
+          <h2> DESARROLLO WEB</h2>
+        </SDesarrolloWeb>
+        <SDesarrolloTexto>
+          {" "}
+          <h3>
+            {" "}
+            lorem ipsum dolor sit amet, consectetur adipsing elit, sed do
+            eiusmod tempor incididut.{" "}
+          </h3>{" "}
+        </SDesarrolloTexto>
+        <SDesarrolloMas>
+          <Plus stroke={colors.black} className={"svgPlus"} />
+        </SDesarrolloMas>
 
-<SContenedorTitulo></SContenedorTitulo>
-<SContenedorGrid></SContenedorGrid>
+        <SCreacionContenido>
+          <h2> CREACION DE CONTENIDO</h2>
+        </SCreacionContenido>
+        <SCreaciontexto>
+          <h3>
+            {" "}
+            lorem ipsum dolor sit amet, consectetur adipsing elit, sed do
+            eiusmod tempor incididut.{" "}
+          </h3>{" "}
+        </SCreaciontexto>
+        <SCreacionMas>  <Plus stroke={colors.black} className={"svgPlus"} /></SCreacionMas>
+      </SContenedorGrid>
+    </SContenedorInicial>
+  );
+};
 
-</SContenedorInicial>
-      
-    )
-}
-
-export default Call
+export default Call;
--- src/styles/js/servicios.js
+++ src/styles/js/servicios.js
@@ -1,20 +1,19 @@
 import styled from "styled-components"
 import { above } from './index'
 import { colors, fonts, sizes } from "../../utils/const"
+import { color } from "framer-motion"
 
 
 export const SContenedorInicial = styled.div`
 width: 100vw;
 height: 80vh; 
-background-color:red;  
+background-color:${colors.white};    
 display: flex;
 justify-content:space-around;
 align-items: center;
 flex-direction: column;
 
 
-
-
 ${above.medium`
     
   `}
@@ -22,16 +21,23 @@ ${above.large`
 
   `}
 `
+
 
 export const SContenedorTitulo = styled.div`
 width: 30%;
 height: 16%; 
-background-color:white;  
+background-color:${colors.white};  
 display: flex;
 justify-content: center;
 align-items: center;
+padding: 10% 0 0;
 
+h1{
+font-family: ${fonts.reciaB};
+color: ${colors.black};
+font-size: 3.5em;
 
+}
 
 
 ${above.medium`
@@ -39,13 +45,123 @@ ${above.medium`
   `}
 ${above.large`
 
+h1{
+
+font-size: 5em;
+
+}
   `}
 `
 
 export const SContenedorGrid = styled.div`
-width: 95%;
+width: 85%;
 height: 80%; 
-background-color:black;  
+background-color: none;
+display: grid;
+grid-template-columns:70% 20%;
+grid-template-rows: repeat(6, 15%);
+grid-column-gap: 0px;
+grid-row-gap: 0px;
+
+
+
+${above.medium`
+    
+  `}
+${above.large`
+grid-template-columns: 30% 60% 10%;
+grid-template-rows: repeat(3, 1fr);
+grid-column-gap: 0px;
+grid-row-gap: 0px;
+padding: 3% 0 0;
+
+  `}
+`
+
+
+export const SBranding = styled.div`
+
+background-color:${colors.white};    
+grid-area:  1 / 1 / 2 / 2;
+border-top: solid 0.1em ${colors.black};
+border-bottom: solid 0.1em ${colors.black};
+
+display: flex;
+justify-content:flex-start ;
+align-items: center;
+padding: 0 0 0 15%;
+
+h2{
+  font-family: ${fonts.reciaB};
+  font-size: 2em;
+  color: ${colors.black};
+}
+
+
+
+${above.medium`
+    
+  `}
+${above.large`
+
+border-bottom:none;
+grid-area: 1 / 1 / 2 / 2;
+display: flex;
+justify-content:flex-start;
+align-items: center;
+padding: 0 0 0 15%;
+
+h2{
+  font-family: ${fonts.reciaB};
+  font-size: 2em;
+  color: ${colors.black};
+}
+
+  `}
+`
+
+export const SBrandingTexto = styled.div`
+
+background-color:${colors.white};    
+grid-area: 2 / 1 / 3 / 3;
+
+display: flex;
+justify-content:flex-start;
+align-items: center;
+padding: 0 0 0 10%;
+
+h3{
+  font-family: ${fonts.monttMedium};
+  font-size: 1.1em;
+  color: ${colors.black};
+
+}
+
+${above.medium`
+    
+  `}
+${above.large`
+background-color:${colors.white};    
+grid-area: 1 / 2 / 2 / 3; 
+border-top: solid 0.1em ${colors.black};
+display: flex;
+justify-content:flex-start;
+align-items: center;
+padding: 0 0 0 10%;
+
+h3{
+  font-family: ${fonts.monttMedium};
+  font-size: 1.1em;
+  color: ${colors.black};
+  `}
+`
+
+export const SBrandingMas = styled.div`
+
+background-color:${colors.white};    
+grid-area: 1 / 2 / 2 / 3;
+border-top: solid 0.1em ${colors.black};
+border-bottom: solid 0.1em ${colors.black};
 display: flex;
 justify-content: center;
 align-items: center;
@@ -56,6 +172,167 @@ ${above.medium`
     
   `}
 ${above.large`
+
+border-bottom: none;
+grid-area: 1 / 3 / 2 / 4;
+border-top: solid 0.1em ${colors.black};
+display: flex;
+justify-content: center;
+align-items: center;
+
+  `}
+`
+
+export const SDesarrolloWeb = styled.div`
+
+background-color:${colors.white};    
+grid-area: 3 / 1 / 4 / 2; 
+border-top: solid 0.1em ${colors.black};
+border-bottom: none;
+
+display: flex;
+justify-content:flex-start;
+align-items: center;
+padding: 0 0 0 15%;
+
+h2{
+  font-family: ${fonts.reciaB};
+  font-size: 2em;
+  color: ${colors.black};
+}
+
+
+${above.medium`
+    
+  `}
+${above.large`
+
+border-bottom: none;
+
+grid-area: 2 / 1 / 3 / 2;
+  `}
+`
+export const SDesarrolloTexto = styled.div`
+
+background-color:${colors.white};    
+grid-area: 4 / 1 / 5 / 3;
+border-top: solid 0.1em ${colors.black};
+display: flex;
+justify-content:flex-start;
+align-items: center;
+padding: 0 0 0 10%;
+
+h3{
+  font-family: ${fonts.monttMedium};
+  font-size: 1.1em;
+  color: ${colors.black};
+
+}
+
+
+
+${above.medium`
+    
+  `}
+${above.large`
+grid-area: 2 / 2 / 3 / 3;
+  `}
+`
+
+export const SDesarrolloMas = styled.div`
+
+background-color:${colors.white};    
+grid-area: 3 / 2 / 4 / 3;
+border-top: solid 0.1em ${colors.black};
+display: flex;
+justify-content: center;
+align-items: center;
+
+
+
+${above.medium`
+    
+  `}
+${above.large`
+grid-area: 2 / 3 / 3 / 4;
+  `}
+`
+
+export const SCreacionContenido = styled.div`
+
+background-color:${colors.white};    
+grid-area: 5 / 1 / 6 / 2;
+ border-top: solid 0.1em ${colors.black};
+ border-bottom: solid 0.1em ${colors.black};
+display: flex;
+justify-content:flex-start;
+align-items: center;
+padding: 5% 0 0 15%;
+
+h2{
+  font-family: ${fonts.reciaB};
+  font-size: 2em;
+  color: ${colors.black};
+}
+ 
+
+
+
+${above.medium`
+    
+  `}
+${above.large`
+grid-area: 3 / 1 / 4 / 2;
+  `}
+`
+
+export const SCreaciontexto = styled.div`
+
+background-color:${colors.white};    
+grid-area: 6 / 1 / 7 / 3;
+display: flex;
+justify-content:flex-start;
+align-items: center;
+padding: 0 0 0 10%;
+
+h3{
+  font-family: ${fonts.monttMedium};
+  font-size: 1.1em;
+  color: ${colors.black};
+
+}
+
+
+
+${above.medium`
+    
+  `}
+${above.large`
+border-top: solid 0.1em ${colors.black};
+border-bottom: solid 0.1em ${colors.black};
+grid-area: 3 / 2 / 4 / 3;
+  `}
+`
+
+
+export const SCreacionMas= styled.div`
+
+background-color:${colors.white};    
+grid-area: 5 / 2 / 6 / 3;
+border-top: solid 0.1em ${colors.black};
+border-bottom: solid 0.1em ${colors.black};
+display: flex;
+justify-content: center;
+align-items: center;
+
+
+
+${above.medium`
+    
+  `}
+${above.large`
+
+grid-area: 3 / 3 / 4 / 4;
 
   `}
 `
