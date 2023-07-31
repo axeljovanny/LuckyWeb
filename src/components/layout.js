1aa4b240f286a87da8d8858d0c3f172799f69296
Axel <axel.jovanny.aj@gmail.com>
2023-07-31 13:18:13 -0600

    correccion de libreria React

--- src/components/layout.js
+++ src/components/layout.js
@@ -21,9 +21,6 @@ const Layout = ({ children }) => {
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes, viewport-fit=cover" />
-
-        <script src="https://cdn.jsdelivr.net/npm/react@17.0.2/umd/react.development.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/react-dom@17.0.2/umd/react-dom.development.js"></script>
       </head>
 
       <GlobalStyles />
